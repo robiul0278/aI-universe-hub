788e361225bf8b66075964ccb4ea72f951423c79
robiul0278 <robiul0278@gmail.com>
2023-03-03 21:53:37 +0600

    add modal temolete

--- api.js
+++ api.js
@@ -1,15 +1,28 @@
 console.log('loded')
 
-const loadAi = async() => {
-    // const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
-    const url = `https://openapi.programming-hero.com/api/ai/tools`;
-    const res = await fetch(url);
-    const data = await res.json();
-    displayAi(data.data.tools);
+const loadAi = () =>{
+    fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then(res => res.json())
+    .then(data => displayAi(data.data.tools.slice(0, 6)))
 }
 
+
+
 const displayAi = aiHubs =>{
+
+// Remove Show all button
+    const showAll = document.getElementById('show-all')
+    if(aiHubs.length < 10){  
+    showAll.classList.remove('d-none')
+    }
+    else{
+    showAll.classList.add('d-none')
+    }
+
+
     const aiContainer = document.getElementById('ai-container');
+    aiContainer.innerHTML = ''; 
+    toggleSpinner(true);
     aiHubs.forEach(aiHub =>{
         console.log(aiHub)
         const aiDiv = document.createElement('div');
@@ -22,20 +35,43 @@ const displayAi = aiHubs =>{
             <div>
                 <p>1.${aiHub.features[0]}</p>
                 <p>2.${aiHub.features[1]}</p>
-                <p>3.${aiHub.features[2]}</p>
+                <p>3.${aiHub.features[2] ? aiHub.features[2] : "No data found!" }</p>
             </div>
         </div>
         <div class="card-footer">
-            <h5>${aiHub.name}</h5>
-            <div>
+            <div class="d-flex align-items-center">
+                <div>
+                <h5>${aiHub.name}</h5>
                 <i class="bi bi-calendar3"></i>
-                <span>date</span>
+                <span>${aiHub.published_in}</span>
+                </div>
+                <button data-bs-toggle="modal" data-bs-target="#exampleModal" class="position-absolute end-0 mx-3 text-danger  px-1 bg-danger-subtle bg-opacity-10 rounded-circle border-none border border-0 fs-6"><i class="bi bi-arrow-right"></i></button>
             </div>
         </div>
       </div>
         `;
         aiContainer.appendChild(aiDiv);
     })
+    toggleSpinner(false);
 }
 
+
+// toggleSpinner =============================================
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none')
+    }
+    else{
+        loaderSection.classList.add('d-none')
+    }
+}
+
+
 loadAi()
+
+const showAllData = () => {
+    fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then(res => res.json())
+    .then(data => displayAi(data.data.tools))
+}
